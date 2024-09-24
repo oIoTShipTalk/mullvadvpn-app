a1b9c7645b18e838998b76c8f6408704fb483d82
Oskar <oskar@mullvad.net>
2024-09-24 08:26:06 +0200

    Move settings from VPN settings to General

--- gui/src/renderer/components/GeneralSettings.tsx
+++ gui/src/renderer/components/GeneralSettings.tsx
@@ -2,14 +2,19 @@ import { useCallback } from 'react';
 import styled from 'styled-components';
 
 import { messages } from '../../shared/gettext';
+import log from '../../shared/logging';
 import { useAppContext } from '../context';
 import { useHistory } from '../lib/history';
 import { RoutePath } from '../lib/routes';
+import { useBoolean } from '../lib/utilityHooks';
 import { useSelector } from '../redux/store';
-import { AriaDescription, AriaInput, AriaInputGroup, AriaLabel } from './AriaGroup';
+import * as AppButton from './AppButton';
+import { AriaDescription, AriaDetails, AriaInput, AriaInputGroup, AriaLabel } from './AriaGroup';
 import * as Cell from './cell';
+import InfoButton, { InfoIcon } from './InfoButton';
 import { BackAction } from './KeyboardNavigation';
 import { Layout, SettingsContainer } from './Layout';
+import { ModalAlert, ModalAlertType, ModalMessage } from './Modal';
 import {
   NavigationBar,
   NavigationContainer,
@@ -34,6 +39,10 @@ const StyledAnimateMapSettingsGroup = styled(Cell.Group)({
   '@media (prefers-reduced-motion: reduce)': {
     display: 'none',
   },
+});
+
+const StyledInfoIcon = styled(InfoIcon)({
+  marginRight: '16px',
 });
 
 export default function GeneralSettings() {
@@ -64,6 +73,16 @@ export default function GeneralSettings() {
               </SettingsHeader>
 
               <StyledContent>
+                <Cell.Group>
+                  <AutoStart />
+                  <AutoConnect />
+                </Cell.Group>
+
+                <Cell.Group>
+                  <KillSwitchInfo />
+                  <LockdownMode />
+                </Cell.Group>
+
                 <Cell.Group>
                   <NotificationsSetting />
                 </Cell.Group>
@@ -97,6 +116,206 @@ export default function GeneralSettings() {
         </SettingsContainer>
       </Layout>
     </BackAction>
+  );
+}
+
+function AutoStart() {
+  const autoStart = useSelector((state) => state.settings.autoStart);
+  const { setAutoStart: setAutoStartImpl } = useAppContext();
+
+  const setAutoStart = useCallback(
+    async (autoStart: boolean) => {
+      try {
+        await setAutoStartImpl(autoStart);
+      } catch (e) {
+        const error = e as Error;
+        log.error(`Cannot set auto-start: ${error.message}`);
+      }
+    },
+    [setAutoStartImpl],
+  );
+
+  return (
+    <AriaInputGroup>
+      <Cell.Container>
+        <AriaLabel>
+          <Cell.InputLabel>
+            {messages.pgettext('vpn-settings-view', 'Launch app on start-up')}
+          </Cell.InputLabel>
+        </AriaLabel>
+        <AriaInput>
+          <Cell.Switch isOn={autoStart} onChange={setAutoStart} />
+        </AriaInput>
+      </Cell.Container>
+    </AriaInputGroup>
+  );
+}
+
+function AutoConnect() {
+  const autoConnect = useSelector((state) => state.settings.guiSettings.autoConnect);
+  const { setAutoConnect } = useAppContext();
+
+  return (
+    <AriaInputGroup>
+      <Cell.Container>
+        <AriaLabel>
+          <Cell.InputLabel>
+            {messages.pgettext('vpn-settings-view', 'Auto-connect')}
+          </Cell.InputLabel>
+        </AriaLabel>
+        <AriaInput>
+          <Cell.Switch isOn={autoConnect} onChange={setAutoConnect} />
+        </AriaInput>
+      </Cell.Container>
+      <Cell.CellFooter>
+        <AriaDescription>
+          <Cell.CellFooterText>
+            {messages.pgettext(
+              'vpn-settings-view',
+              'Automatically connect to a server when the app launches.',
+            )}
+          </Cell.CellFooterText>
+        </AriaDescription>
+      </Cell.CellFooter>
+    </AriaInputGroup>
+  );
+}
+
+function KillSwitchInfo() {
+  const [killSwitchInfoVisible, showKillSwitchInfo, hideKillSwitchInfo] = useBoolean(false);
+
+  return (
+    <>
+      <Cell.CellButton onClick={showKillSwitchInfo}>
+        <AriaInputGroup>
+          <AriaLabel>
+            <Cell.InputLabel>
+              {messages.pgettext('vpn-settings-view', 'Kill switch')}
+            </Cell.InputLabel>
+          </AriaLabel>
+          <StyledInfoIcon />
+          <AriaInput>
+            <Cell.Switch isOn disabled />
+          </AriaInput>
+        </AriaInputGroup>
+      </Cell.CellButton>
+      <ModalAlert
+        isOpen={killSwitchInfoVisible}
+        type={ModalAlertType.info}
+        buttons={[
+          <AppButton.BlueButton key="back" onClick={hideKillSwitchInfo}>
+            {messages.gettext('Got it!')}
+          </AppButton.BlueButton>,
+        ]}
+        close={hideKillSwitchInfo}>
+        <ModalMessage>
+          {messages.pgettext(
+            'vpn-settings-view',
+            'This built-in feature prevents your traffic from leaking outside of the VPN tunnel if your network suddenly stops working or if the tunnel fails, it does this by blocking your traffic until your connection is reestablished.',
+          )}
+        </ModalMessage>
+        <ModalMessage>
+          {messages.pgettext(
+            'vpn-settings-view',
+            'The difference between the Kill Switch and Lockdown Mode is that the Kill Switch will prevent any leaks from happening during automatic tunnel reconnects, software crashes and similar accidents. With Lockdown Mode enabled, you must be connected to a Mullvad VPN server to be able to reach the internet. Manually disconnecting or quitting the app will block your connection.',
+          )}
+        </ModalMessage>
+      </ModalAlert>
+    </>
+  );
+}
+
+function LockdownMode() {
+  const blockWhenDisconnected = useSelector((state) => state.settings.blockWhenDisconnected);
+  const { setBlockWhenDisconnected: setBlockWhenDisconnectedImpl } = useAppContext();
+
+  const [confirmationDialogVisible, showConfirmationDialog, hideConfirmationDialog] =
+    useBoolean(false);
+
+  const setBlockWhenDisconnected = useCallback(
+    async (blockWhenDisconnected: boolean) => {
+      try {
+        await setBlockWhenDisconnectedImpl(blockWhenDisconnected);
+      } catch (e) {
+        const error = e as Error;
+        log.error('Failed to update block when disconnected', error.message);
+      }
+    },
+    [setBlockWhenDisconnectedImpl],
+  );
+
+  const setLockDownMode = useCallback(
+    async (newValue: boolean) => {
+      if (newValue) {
+        showConfirmationDialog();
+      } else {
+        await setBlockWhenDisconnected(false);
+      }
+    },
+    [setBlockWhenDisconnected, showConfirmationDialog],
+  );
+
+  const confirmLockdownMode = useCallback(async () => {
+    hideConfirmationDialog();
+    await setBlockWhenDisconnected(true);
+  }, [hideConfirmationDialog, setBlockWhenDisconnected]);
+
+  return (
+    <>
+      <AriaInputGroup>
+        <Cell.Container>
+          <AriaLabel>
+            <Cell.InputLabel>
+              {messages.pgettext('vpn-settings-view', 'Lockdown mode')}
+            </Cell.InputLabel>
+          </AriaLabel>
+          <AriaDetails>
+            <InfoButton>
+              <ModalMessage>
+                {messages.pgettext(
+                  'vpn-settings-view',
+                  'The difference between the Kill Switch and Lockdown Mode is that the Kill Switch will prevent any leaks from happening during automatic tunnel reconnects, software crashes and similar accidents.',
+                )}
+              </ModalMessage>
+              <ModalMessage>
+                {messages.pgettext(
+                  'vpn-settings-view',
+                  'With Lockdown Mode enabled, you must be connected to a Mullvad VPN server to be able to reach the internet. Manually disconnecting or quitting the app will block your connection.',
+                )}
+              </ModalMessage>
+            </InfoButton>
+          </AriaDetails>
+          <AriaInput>
+            <Cell.Switch isOn={blockWhenDisconnected} onChange={setLockDownMode} />
+          </AriaInput>
+        </Cell.Container>
+      </AriaInputGroup>
+      <ModalAlert
+        isOpen={confirmationDialogVisible}
+        type={ModalAlertType.caution}
+        buttons={[
+          <AppButton.RedButton key="confirm" onClick={confirmLockdownMode}>
+            {messages.gettext('Enable anyway')}
+          </AppButton.RedButton>,
+          <AppButton.BlueButton key="back" onClick={hideConfirmationDialog}>
+            {messages.gettext('Back')}
+          </AppButton.BlueButton>,
+        ]}
+        close={hideConfirmationDialog}>
+        <ModalMessage>
+          {messages.pgettext(
+            'vpn-settings-view',
+            'Attention: enabling this will always require a Mullvad VPN connection in order to reach the internet.',
+          )}
+        </ModalMessage>
+        <ModalMessage>
+          {messages.pgettext(
+            'vpn-settings-view',
+            'The app’s built-in kill switch is always on. This setting will additionally block the internet if clicking Disconnect or Quit.',
+          )}
+        </ModalMessage>
+      </ModalAlert>
+    </>
   );
 }
 
--- gui/src/renderer/components/VpnSettings.tsx
+++ gui/src/renderer/components/VpnSettings.tsx
@@ -11,18 +11,16 @@ import { useRelaySettingsUpdater } from '../lib/constraint-updater';
 import { useHistory } from '../lib/history';
 import { formatHtml } from '../lib/html-formatter';
 import { RoutePath } from '../lib/routes';
-import { useBoolean } from '../lib/utilityHooks';
 import { RelaySettingsRedux } from '../redux/settings/reducers';
 import { useSelector } from '../redux/store';
-import * as AppButton from './AppButton';
 import { AriaDescription, AriaDetails, AriaInput, AriaInputGroup, AriaLabel } from './AriaGroup';
 import * as Cell from './cell';
 import Selector, { SelectorItem } from './cell/Selector';
 import CustomDnsSettings from './CustomDnsSettings';
-import InfoButton, { InfoIcon } from './InfoButton';
+import InfoButton from './InfoButton';
 import { BackAction } from './KeyboardNavigation';
 import { Layout, SettingsContainer } from './Layout';
-import { ModalAlert, ModalAlertType, ModalMessage } from './Modal';
+import { ModalMessage } from './Modal';
 import {
   NavigationBar,
   NavigationContainer,
@@ -37,10 +35,6 @@ const StyledContent = styled.div({
   flexDirection: 'column',
   flex: 1,
   marginBottom: '2px',
-});
-
-const StyledInfoIcon = styled(InfoIcon)({
-  marginRight: '16px',
 });
 
 const StyledSelectorContainer = styled.div({
@@ -90,11 +84,6 @@ export default function VpnSettings() {
 
               <StyledContent>
                 <Cell.Group>
-                  <AutoStart />
-                  <AutoConnect />
-                </Cell.Group>
-
-                <Cell.Group>
                   <AllowLan />
                 </Cell.Group>
 
@@ -104,11 +93,6 @@ export default function VpnSettings() {
 
                 <Cell.Group>
                   <EnableIpv6 />
-                </Cell.Group>
-
-                <Cell.Group>
-                  <KillSwitchInfo />
-                  <LockdownMode />
                 </Cell.Group>
 
                 <Cell.Group>
@@ -133,68 +117,6 @@ export default function VpnSettings() {
         </SettingsContainer>
       </Layout>
     </BackAction>
-  );
-}
-
-function AutoStart() {
-  const autoStart = useSelector((state) => state.settings.autoStart);
-  const { setAutoStart: setAutoStartImpl } = useAppContext();
-
-  const setAutoStart = useCallback(
-    async (autoStart: boolean) => {
-      try {
-        await setAutoStartImpl(autoStart);
-      } catch (e) {
-        const error = e as Error;
-        log.error(`Cannot set auto-start: ${error.message}`);
-      }
-    },
-    [setAutoStartImpl],
-  );
-
-  return (
-    <AriaInputGroup>
-      <Cell.Container>
-        <AriaLabel>
-          <Cell.InputLabel>
-            {messages.pgettext('vpn-settings-view', 'Launch app on start-up')}
-          </Cell.InputLabel>
-        </AriaLabel>
-        <AriaInput>
-          <Cell.Switch isOn={autoStart} onChange={setAutoStart} />
-        </AriaInput>
-      </Cell.Container>
-    </AriaInputGroup>
-  );
-}
-
-function AutoConnect() {
-  const autoConnect = useSelector((state) => state.settings.guiSettings.autoConnect);
-  const { setAutoConnect } = useAppContext();
-
-  return (
-    <AriaInputGroup>
-      <Cell.Container>
-        <AriaLabel>
-          <Cell.InputLabel>
-            {messages.pgettext('vpn-settings-view', 'Auto-connect')}
-          </Cell.InputLabel>
-        </AriaLabel>
-        <AriaInput>
-          <Cell.Switch isOn={autoConnect} onChange={setAutoConnect} />
-        </AriaInput>
-      </Cell.Container>
-      <Cell.CellFooter>
-        <AriaDescription>
-          <Cell.CellFooterText>
-            {messages.pgettext(
-              'vpn-settings-view',
-              'Automatically connect to a server when the app launches.',
-            )}
-          </Cell.CellFooterText>
-        </AriaDescription>
-      </Cell.CellFooter>
-    </AriaInputGroup>
   );
 }
 
@@ -537,144 +459,6 @@ function EnableIpv6() {
         </AriaInput>
       </Cell.Container>
     </AriaInputGroup>
-  );
-}
-
-function KillSwitchInfo() {
-  const [killSwitchInfoVisible, showKillSwitchInfo, hideKillSwitchInfo] = useBoolean(false);
-
-  return (
-    <>
-      <Cell.CellButton onClick={showKillSwitchInfo}>
-        <AriaInputGroup>
-          <AriaLabel>
-            <Cell.InputLabel>
-              {messages.pgettext('vpn-settings-view', 'Kill switch')}
-            </Cell.InputLabel>
-          </AriaLabel>
-          <StyledInfoIcon />
-          <AriaInput>
-            <Cell.Switch isOn disabled />
-          </AriaInput>
-        </AriaInputGroup>
-      </Cell.CellButton>
-      <ModalAlert
-        isOpen={killSwitchInfoVisible}
-        type={ModalAlertType.info}
-        buttons={[
-          <AppButton.BlueButton key="back" onClick={hideKillSwitchInfo}>
-            {messages.gettext('Got it!')}
-          </AppButton.BlueButton>,
-        ]}
-        close={hideKillSwitchInfo}>
-        <ModalMessage>
-          {messages.pgettext(
-            'vpn-settings-view',
-            'This built-in feature prevents your traffic from leaking outside of the VPN tunnel if your network suddenly stops working or if the tunnel fails, it does this by blocking your traffic until your connection is reestablished.',
-          )}
-        </ModalMessage>
-        <ModalMessage>
-          {messages.pgettext(
-            'vpn-settings-view',
-            'The difference between the Kill Switch and Lockdown Mode is that the Kill Switch will prevent any leaks from happening during automatic tunnel reconnects, software crashes and similar accidents. With Lockdown Mode enabled, you must be connected to a Mullvad VPN server to be able to reach the internet. Manually disconnecting or quitting the app will block your connection.',
-          )}
-        </ModalMessage>
-      </ModalAlert>
-    </>
-  );
-}
-
-function LockdownMode() {
-  const blockWhenDisconnected = useSelector((state) => state.settings.blockWhenDisconnected);
-  const { setBlockWhenDisconnected: setBlockWhenDisconnectedImpl } = useAppContext();
-
-  const [confirmationDialogVisible, showConfirmationDialog, hideConfirmationDialog] =
-    useBoolean(false);
-
-  const setBlockWhenDisconnected = useCallback(
-    async (blockWhenDisconnected: boolean) => {
-      try {
-        await setBlockWhenDisconnectedImpl(blockWhenDisconnected);
-      } catch (e) {
-        const error = e as Error;
-        log.error('Failed to update block when disconnected', error.message);
-      }
-    },
-    [setBlockWhenDisconnectedImpl],
-  );
-
-  const setLockDownMode = useCallback(
-    async (newValue: boolean) => {
-      if (newValue) {
-        showConfirmationDialog();
-      } else {
-        await setBlockWhenDisconnected(false);
-      }
-    },
-    [setBlockWhenDisconnected, showConfirmationDialog],
-  );
-
-  const confirmLockdownMode = useCallback(async () => {
-    hideConfirmationDialog();
-    await setBlockWhenDisconnected(true);
-  }, [hideConfirmationDialog, setBlockWhenDisconnected]);
-
-  return (
-    <>
-      <AriaInputGroup>
-        <Cell.Container>
-          <AriaLabel>
-            <Cell.InputLabel>
-              {messages.pgettext('vpn-settings-view', 'Lockdown mode')}
-            </Cell.InputLabel>
-          </AriaLabel>
-          <AriaDetails>
-            <InfoButton>
-              <ModalMessage>
-                {messages.pgettext(
-                  'vpn-settings-view',
-                  'The difference between the Kill Switch and Lockdown Mode is that the Kill Switch will prevent any leaks from happening during automatic tunnel reconnects, software crashes and similar accidents.',
-                )}
-              </ModalMessage>
-              <ModalMessage>
-                {messages.pgettext(
-                  'vpn-settings-view',
-                  'With Lockdown Mode enabled, you must be connected to a Mullvad VPN server to be able to reach the internet. Manually disconnecting or quitting the app will block your connection.',
-                )}
-              </ModalMessage>
-            </InfoButton>
-          </AriaDetails>
-          <AriaInput>
-            <Cell.Switch isOn={blockWhenDisconnected} onChange={setLockDownMode} />
-          </AriaInput>
-        </Cell.Container>
-      </AriaInputGroup>
-      <ModalAlert
-        isOpen={confirmationDialogVisible}
-        type={ModalAlertType.caution}
-        buttons={[
-          <AppButton.RedButton key="confirm" onClick={confirmLockdownMode}>
-            {messages.gettext('Enable anyway')}
-          </AppButton.RedButton>,
-          <AppButton.BlueButton key="back" onClick={hideConfirmationDialog}>
-            {messages.gettext('Back')}
-          </AppButton.BlueButton>,
-        ]}
-        close={hideConfirmationDialog}>
-        <ModalMessage>
-          {messages.pgettext(
-            'vpn-settings-view',
-            'Attention: enabling this will always require a Mullvad VPN connection in order to reach the internet.',
-          )}
-        </ModalMessage>
-        <ModalMessage>
-          {messages.pgettext(
-            'vpn-settings-view',
-            'The app’s built-in kill switch is always on. This setting will additionally block the internet if clicking Disconnect or Quit.',
-          )}
-        </ModalMessage>
-      </ModalAlert>
-    </>
   );
 }
 
