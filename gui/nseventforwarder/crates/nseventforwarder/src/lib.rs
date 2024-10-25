f298ed695614c5df74916e21c56ee23f72766051
Markus Pettersson <markus.pettersson@mullvad.net>
2024-10-25 10:57:07 +0200

    Add threading

--- gui/nseventforwarder/crates/nseventforwarder/src/lib.rs
+++ gui/nseventforwarder/crates/nseventforwarder/src/lib.rs
@@ -1,8 +1,28 @@
-use std::sync::Mutex;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread::JoinHandle,
+};
 
 use block2::RcBlock;
-use neon::prelude::*;
+use neon::{
+    prelude::{
+        Context, FunctionContext, Handle, JsFunction, JsNull, JsResult, JsUndefined, ModuleContext,
+        NeonResult, Object, Root,
+    },
+    result::Throw,
+};
 use objc2_app_kit::{NSEvent, NSEventMask};
+
+/// NSEventMonitor instance. It must be initialized by `start` and cleaned up by the callback
+/// function returned from `start`.
+static NSEVENTMONITOR: Mutex<Option<NSEventMonitor>> = Mutex::new(None);
+
+struct NSEventMonitor {
+    /// The thread listening for incoming [NSEvent]s.
+    thread: JoinHandle<()>,
+    /// Signal for the current execution context to stop.
+    stop: mpsc::Sender<()>,
+}
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
@@ -11,38 +31,77 @@ fn main(mut cx: ModuleContext) -> NeonResult<()> {
 }
 
 fn start(mut cx: FunctionContext) -> JsResult<JsFunction> {
-    let callback = cx.argument::<JsFunction>(0)?;
+    // Set up neon stuff
+    let callback = cx.argument::<JsFunction>(0)?.root(&mut cx);
+    let callback: Arc<Root<JsFunction>> = Arc::new(callback);
+    let channel = cx.channel();
 
-    let (sender, receiver) = std::sync::mpsc::channel();
+    // Start a long-running thread which handles incoming NS events
+    // When a new event is received, call the callback passed to us from the JavaScript caller
+    let (stop_tx, stop_rx) = mpsc::channel();
+    let join_handle = std::thread::spawn(move || {
+        let callback = Arc::<Root<JsFunction>>::clone(&callback);
+        // Scaffolding for calling the JavaScript callback function
+        let call_callback = move || {
+            let cb = Arc::clone(&callback);
+            channel.send(move |mut cx| {
+                let this = JsNull::new(&mut cx);
+                let _ = cb.to_inner(&mut cx).call(&mut cx, this, []);
+                Ok(())
+            })
+        };
+        // Start monitoring incoming NS events
+        let block = RcBlock::new(move |_event| {
+            call_callback();
+        });
+        // Note: Make sure to cancler this handler with [NSEvent::removeMonitor] to unregister the
+        // listener.
+        let mut handler = unsafe {
+            NSEvent::addGlobalMonitorForEventsMatchingMask_handler(
+                NSEventMask::LeftMouseDown | NSEventMask::RightMouseDown,
+                &block,
+            )
+        };
 
-    let block = RcBlock::new(move |_event| {
-        println!("1 Event received: {:?}", _event);
-        let _ = sender.send(());
+        // Listen for stop signal
+        let _ = stop_rx.recv();
+        if let Some(handler) = handler.take() {
+            unsafe { NSEvent::removeMonitor(&handler) }
+        }
+        // The thread's execution will stop when this function returns
     });
 
-    let _handler = unsafe {
-        NSEvent::addGlobalMonitorForEventsMatchingMask_handler(
-            NSEventMask::LeftMouseDown | NSEventMask::RightMouseDown,
-            &block,
-        )
+    let new_context = NSEventMonitor {
+        thread: join_handle,
+        stop: stop_tx,
     };
 
-    let stop = Mutex::new(false);
+    // Update the global NSEventMonitor state
+    let mut nseventmonitor_context = NSEVENTMONITOR.lock().unwrap();
+    // Stop any old NSEventMonitor
+    if let Some(context) = nseventmonitor_context.take() {
+        let _ = context.stop.send(());
+        context
+            .thread
+            .join()
+            .expect("Couldn't join on the associated thread");
+    }
+    let _ = nseventmonitor_context.insert(new_context);
+    drop(nseventmonitor_context);
 
-    if let Ok(_) = receiver.recv() {
-        let stop = *stop.lock().unwrap();
-        if !stop {
-            // break;
-        }
+    JsFunction::new(&mut cx, stop)
+}
 
-        println!("2 Event received");
-        let this = JsNull::new(&mut cx);
-        let _ = callback.call(&mut cx, this, []);
+fn stop(mut cx: FunctionContext<'_>) -> Result<Handle<'_, JsUndefined>, Throw> {
+    if let Some(context) = NSEVENTMONITOR.lock().unwrap().take() {
+        // Tell the thread to stop running
+        let _ = context.stop.send(());
+        // Wait for the thread to shutdown
+        context
+            .thread
+            .join()
+            .expect("Couldn't join on the associated thread");
     }
 
-    JsFunction::new(&mut cx, move |mut cx| {
-        let mut stop = stop.lock().unwrap();
-        *stop = true;
-        Ok(JsUndefined::new(&mut cx))
-    })
+    Ok(JsUndefined::new(&mut cx))
 }
