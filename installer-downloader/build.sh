595f27041b1ca33fd9ac963722c2df87406968bb
David Lönnhager <david.l@mullvad.net>
2025-02-08 00:30:22 +0100

    fixup! Add build script for installer-downloader

--- installer-downloader/build.sh
+++ installer-downloader/build.sh
@@ -11,7 +11,8 @@
 # lto = true
 # panic = 'abort'
 #
-# We cannot set the above directly in Cargo.toml since it must be set for the entire workspace.
+# We cannot all of set the above directly in Cargo.toml since some must be set for the entire
+# workspace.
 
 set -eu
 
@@ -23,6 +24,5 @@ source ../scripts/utils/host
 # shellcheck disable=SC1091
 source ../scripts/utils/log
 
-RUSTFLAGS="-C codegen-units=1 -C panic=abort -C strip=symbols -C opt-level=z"
-
-RUSTFLAGS=$RUSTFLAGS cargo run --bin installer-downloader --release
+RUSTFLAGS="-C codegen-units=1 -C panic=abort -C strip=symbols -C opt-level=z" \
+    cargo build --bin installer-downloader --release
