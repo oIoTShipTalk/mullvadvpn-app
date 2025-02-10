c034226b9f5014ba339fc4013c13f8d038c5079b
David Lönnhager <david.l@mullvad.net>
2025-02-10 11:36:59 +0100

    Spawn thread for COM library

--- desktop/packages/rust-utils/rust-utils-rs/lib.rs
+++ desktop/packages/rust-utils/rust-utils-rs/lib.rs
@@ -1,5 +1,7 @@
 #![warn(clippy::undocumented_unsafe_blocks)]
 
+use std::sync::{mpsc, OnceLock};
+
 use neon::prelude::*;
 
 use windows::core::{HSTRING, PCWSTR};
@@ -12,58 +14,92 @@ use windows::Win32::UI::Shell::{IShellLinkW, ShellLink};
 
 use windows_core::*;
 
+static THREAD_SENDER: OnceLock<mpsc::Sender<Message>> = OnceLock::new();
+
+enum Message {
+    ResolveShortcut {
+        path: String,
+        result_tx: mpsc::Sender<Result<Option<String>>>,
+    },
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext<'_>) -> NeonResult<()> {
     cx.export_function("readShortcut", read_shortcut)?;
+
     Ok(())
 }
 
 fn read_shortcut(mut cx: FunctionContext<'_>) -> JsResult<'_, JsValue> {
-    thread_local! {
-        static COM: Result<ComContext> = ComContext::new()
-    }
+    let tx = THREAD_SENDER.get_or_init(move || {
+        let (tx, rx) = mpsc::channel();
+
+        std::thread::spawn(move || {
+            let _com = ComContext::new()?;
+
+            while let Ok(msg) = rx.recv() {
+                match msg {
+                    Message::ResolveShortcut { path, result_tx } => {
+                        let _ = result_tx.send(get_shortcut_path(&path));
+                    }
+                }
+            }
+
+            Ok::<_, Error>(())
+        });
+
+        tx
+    });
 
     let link_path = cx.argument::<JsString>(0)?.value(&mut cx);
 
-    let path = get_shortcut_path(&link_path);
+    //let path = get_shortcut_path(&link_path);
+    let (result_tx, result_rx) = mpsc::channel();
+    // TODO: handle err
+    let _ = tx.send(Message::ResolveShortcut {
+        path: link_path,
+        result_tx,
+    });
 
-    match path {
-        Some(path) => Ok(cx.string(path).as_value(&mut cx)),
-        None => Ok(cx.null().as_value(&mut cx)),
+    let Ok(result) = result_rx.recv() else {
+        // TODO
+        return Ok(cx.null().as_value(&mut cx));
+    };
+
+    match result {
+        Ok(Some(path)) => Ok(cx.string(path).as_value(&mut cx)),
+        _ => Ok(cx.null().as_value(&mut cx)),
     }
 }
 
-fn get_shortcut_path(path: &str) -> Option<String> {
+fn get_shortcut_path(path: &str) -> Result<Option<String>> {
     let path = HSTRING::from(path);
     let shell_link_result: Result<IShellLinkW> =
         unsafe { CoCreateInstance(&ShellLink, None, CLSCTX_INPROC_SERVER) };
     let Ok(shell_link) = shell_link_result else {
         // TODO: return err
-        return None;
+        return Ok(None);
     };
     let persist_file_result: Result<IPersistFile> = shell_link.cast();
     let Ok(persist_file) = persist_file_result else {
         // TODO: return err
-        return None;
+        return Ok(None);
     };
     unsafe {
-        // TODO: return err
-        persist_file.Load(PCWSTR(path.as_ptr()), STGM_READ).ok()?;
+        persist_file.Load(PCWSTR(path.as_ptr()), STGM_READ)?;
     }
 
     // TODO: what's a good len?
     let mut target_buffer = [0u16; 2 * 1024];
     unsafe {
-        shell_link
-            .GetPath(
-                &mut target_buffer,
-                std::ptr::null_mut(),
-                SLGP_UNCPRIORITY.0 as u32,
-            )
-            .ok()?;
+        shell_link.GetPath(
+            &mut target_buffer,
+            std::ptr::null_mut(),
+            SLGP_UNCPRIORITY.0 as u32,
+        )?;
     }
 
-    strip_null_terminator(&target_buffer)
+    Ok(strip_null_terminator(&target_buffer))
 }
 
 fn strip_null_terminator(slice: &[u16]) -> Option<String> {
